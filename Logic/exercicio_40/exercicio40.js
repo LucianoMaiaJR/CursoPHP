67e0be76b4e49f2a3edd744cefa9da79559dcd61
Luciano Maia <lucianomaia@noclaftech.com>
2024-05-25 16:11:52 -0300

    feat: exercises 40 and 41

--- Logic/exercicio_40/exercicio40.js
+++ Logic/exercicio_40/exercicio40.js
@@ -1,5 +1,17 @@
 /*
-40. Construa uma página/programa onde o usuário digitará dez números e o programa somará 
+40- Construa uma página/programa onde o usuário digitará dez números e o programa somará 
 e calculará a média dos números digitados.
 */
+let numbs = [];
+let count = 0;
 
+while(count < 10) {
+    let number = Number(prompt("selecione um número: "));
+    numbs.push(number);
+    count++
+}
+
+let sum = (((((((((numbs[0] +  numbs[1]) + numbs[2]) + numbs[3]) + numbs[4]) + numbs[5]) + numbs[6]) + numbs[7]) + numbs[8]) + numbs[9]); 
+let result = sum / count
+
+alert(result)
--- Logic/exercicio_41/exercicio41.js
+++ Logic/exercicio_41/exercicio41.js
@@ -3,3 +3,16 @@
 o programa escreverá na tela qual deles é o maior e qual deles é o menor.
 */
 
+let numbs = [];
+let count = 0;
+
+while(count < 8) {
+    let number = Number(prompt("selecione um número: "));
+    numbs.push(number);
+    count++
+}
+
+let maior = Math.max(...numbs);
+let menor = Math.min(...numbs);
+
+alert("Os números digitados foram: " + numbs.join(", ") + "\nO maior número é: " + maior + "\nO menor número é: " + menor);
