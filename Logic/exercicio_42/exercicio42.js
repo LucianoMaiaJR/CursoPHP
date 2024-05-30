43ffa1d4ed0f9adedcf387d495d6400a0cfaa423
Luciano Maia <lucianomaia@noclaftech.com>
2024-05-30 14:02:43 -0300

    feat: exercise 42 logic

--- Logic/exercicio_42/exercicio42.js
+++ Logic/exercicio_42/exercicio42.js
@@ -3,3 +3,18 @@
 O programa deverá calcular quantos deles são maiores que dez.
 */
 
+let numbs = [];
+let count = 0;
+
+while(count < 8) {
+    let number = Number(prompt("selecione um número: "));
+    numbs.push(number);
+    count++
+}
+
+let maioresQueDez = numbs.filter(num => num > 10);
+
+alert("Os números digitados foram: " + numbs.join(", ") + "\nOs números maiores que 10 são: " + (maioresQueDez.length > 0 ? maioresQueDez.join(", ") : "Nenhum"));
+
+
+
